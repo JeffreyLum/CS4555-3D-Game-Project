8e4f2d3b0ba27a19a6cbef9fdf3d091b10e7aed1
Yunjian Lu <43796475+yunjianlu@users.noreply.github.com>
2021-11-16 23:31:03 -0800

    Added second scene, modified ScoreKeeper script that can load next scene

--- Garbage Hunter/Assets/Scripts/ScoreKeeper.cs
+++ Garbage Hunter/Assets/Scripts/ScoreKeeper.cs
@@ -2,21 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ScoreKeeper : MonoBehaviour
 {
     public PlayerMovement player;
+    int cur_Score = 0;
+    string myText = "0";
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        float interval = 0.1f;
+        // keep track of score
+        InvokeRepeating("checkScore", 2, interval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        int input = player.getPoint();
-        gameObject.GetComponent<Text>().text = "" + input;
+        gameObject.GetComponent<Text>().text = myText;
     }
 
+
+    void checkScore()
+    {
+        cur_Score = player.getPoint();
+        // if curent score is greate than 20: move to next level
+        if (cur_Score > 20)   // 20 can be change for your choice
+        {
+            CancelInvoke();
+            invokeWon();
+            cur_Score = 0;
+            Debug.Log("cancel checking score");
+        }
+    }
+
+    // to display text and invoke scence load. 
+    void invokeWon()
+    {
+        gameObject.GetComponent<Text>().color = Color.green;
+        gameObject.GetComponent<Text>().fontSize = 25;
+        //  Giving player 5 seconds to be ready 
+        myText = "You Won! Next Challenge in 5 Sec";
+        InvokeRepeating("loadNextScene", 5, 5);
+        Debug.Log("calling Next scece");
+
+    }
+
+    void loadNextScene()
+    {
+        Debug.Log("loading Next scece");
+        CancelInvoke();
+        // load next 
+        SceneManager.LoadScene(2);
+    }
 }
